c0238f266d6ca21e293675feb879ed7cd535983f
JosephTeteE <josephteteetisong@gmail.com>
2025-02-28 21:12:26 +0100

    Livestream fix

--- server/db.js
+++ server/db.js
@@ -1,5 +1,9 @@
+// server/db.js
+// Description: This file contains the code to connect to the MySQL database.
+// The code uses the mysql2/promise library to connect to the database.
+// The code reads the database connection details from the .env file.
+// The code exports the connection pool which can be used to execute queries on the database.
 const mysql = require("mysql2/promise");
-const fs = require("fs");
 require("dotenv").config();
 
 const pool = mysql.createPool({
@@ -8,12 +12,12 @@ const pool = mysql.createPool({
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
+  ssl: {
+    ca: Buffer.from(process.env.DB_SSL_CERT_BASE64, "base64").toString("utf-8"),
+  },
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
-  ssl: {
-    ca: fs.readFileSync(process.env.DB_SSL_CERT), // Load SSL certificate for secure connection
-  },
 });
 
 module.exports = pool;
